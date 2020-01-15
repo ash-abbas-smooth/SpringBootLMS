0be14a74bcf512607b9362ec34bb90a4b42b4940
ash-abbas-smooth <58793167+ash-abbas-smooth@users.noreply.github.com>
2020-01-15 15:15:28 -0500

    lms orch initialized

---
+++ lms-orchs/src/main/java/com/smoothstack/avalanche/lms/lmsorchs/LmsorchsApplication.java
@@ -0,0 +1,13 @@
+package com.smoothstack.avalanche.lms.lmsorchs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsorchsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsorchsApplication.class, args);
+	}
+
+}
---
+++ lms-orchs/src/test/java/com/smoothstack/avalanche/lms/lmsorchs/LmsorchsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.avalanche.lms.lmsorchs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsorchsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
