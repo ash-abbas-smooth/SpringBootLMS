233cfa9e84ff80d0e08a361c20099f7402b1b1e3
ash-abbas-smooth <58793167+ash-abbas-smooth@users.noreply.github.com>
2020-01-16 15:02:13 -0500

    lms-orchs create

---
+++ lms-orchs/src/main/java/com/smoothstack/avalanche/lmsorchs/LmsOrchsApplication.java
@@ -0,0 +1,22 @@
+package com.smoothstack.avalanche.lmsorchs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class LmsOrchsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsOrchsApplication.class, args);
+	}
+
+	@Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ lms-orchs/src/test/java/com/smoothstack/avalanche/lmsorchs/LmsOrchsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.avalanche.lmsorchs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsOrchsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
